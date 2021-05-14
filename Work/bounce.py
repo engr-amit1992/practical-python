1b6e54979df97b267efc94c9174a4efbfc8e91a5
engr-amit1992 <84131249+engr-amit1992@users.noreply.github.com>
2021-05-14 19:56:20 +0530

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
+
+height=100 # meters
+Dec_in_height= 3/5
+#Total number of bounnces are 10.
+fall=0
+
+while fall<10:
+    height=height*3/5
+    print(round(height,4))
+    fall=fall+1
+
 # Exercise 1.5
